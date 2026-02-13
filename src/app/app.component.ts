e530d76fe6b0900d3a6f9e63a5fc7db7930b2c6a
Taylan Yildirim <taylanyildirim.506@gmail.com>
2026-02-13 10:54:04 +0100

    initiliazed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatCheckbox} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCheckbox],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
