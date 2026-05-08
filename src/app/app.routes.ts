b4f1263e8bb34286a34a4fa9adcf8b73d1c7a195
Taylan Yildirim <taylanyildirim.506@gmail.com>
2026-05-08 18:18:36 +0200

    commit und push um mergen zu können

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {CustomerSucheComponent} from './components/customer-suche/customer-suche.component';
 import {CustomerOverviewPageComponent} from './components/customer-overview-page/customer-overview-page.component';
 import {CustomerErstellenComponent} from './components/customer-erstellen/customer-erstellen.component';
-import {AppComponent} from './app.component';
 import {CustomerUpdateComponent} from './components/customer-update/customer-update.component';
 import {CustomerDeleteComponent} from './components/customer-delete/customer-delete.component';
 import {ReadingComponent} from './components/reading/reading.component';
